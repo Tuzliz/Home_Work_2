360dd10072a54c62687fef6175012ad05e619870
liza <lizashatuzhilkina@gmail.com>
2022-05-13 22:31:41 +0300

    ex

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -2,3 +2,16 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+
+int number = new Random().Next(100,1000);
+string strr = number.ToString();
+
+if (number > 100 && number < 1000)
+{
+    Console.WriteLine($"{number} -> {strr[2]}");
+}
+    
+
+else if (number < 100)
+    Console.WriteLine("третьей цифры нет");
--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -2,3 +2,24 @@
 // 6 -> да 
 // 7 -> да 
 //1 -> нет
+
+//if ((day >= 1 && day <=5 )
+Console.WriteLine ("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day >= 1 && day <=5 )
+Console.WriteLine("Нет");
+else if (day >= 6 || day <= 7)
+Console.WriteLine("Да");
+
+
+
+
+
+
+
+
+
+
+
+
+
